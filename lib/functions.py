9976ba0ae81e66c9af584c9cd427e3caf00b0bb5
blairtchalla821@gmail.com <blairtchalla821@gmail.com>
2024-05-22 14:52:32 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
 
+from ast import Param
+
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    return greet_programmer
+  
 
 def greet(name):
-    pass
+    print (f"Hello, {name}!")
+    return greet
+    
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    return greet_with_default
+
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    print(sum)
+    return sum
+
+add(1, 2)
 
 def halve(number):
-    pass
+    if isinstance(number, int):
+        return number / 2
+    else:
+        return None
+    
+    
+    
+
